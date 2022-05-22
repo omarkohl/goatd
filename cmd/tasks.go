ae0a5b0e6aeae2278819d654751d4e58f78ebb43
Omar Kohl <omarkohl@posteo.net>
2022-05-22 14:38:59 +0200

    Create baseDir with basic content when invoking 'list tasks'

--- cmd/tasks.go
+++ cmd/tasks.go
@@ -2,22 +2,63 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path"
 
 	"github.com/spf13/cobra"
 )
 
+func initBaseDir() error {
+	// This initialization for the baseDir should go somewhere else,
+	// but it's not entirely clear to me where the best place is
+	// Maybe PersistentPreRun .
+	if baseDir == "" {
+		var home string
+		var err error
+		if home, err = os.UserHomeDir(); err != nil {
+			return fmt.Errorf("can't get home dir: %w", err)
+		}
+		baseDir = path.Join(home, "goatd")
+	}
+	if err := os.MkdirAll(baseDir, 0755); err != nil {
+		return fmt.Errorf("can't create baseDir %v: %w", baseDir, err)
+	}
+	dirs := []string{
+		"projects",
+		"someday",
+		"trash",
+		"done",
+	}
+	for _, d := range dirs {
+		d = path.Join(baseDir, d)
+		if err := os.MkdirAll(d, 0755); err != nil {
+			return fmt.Errorf("can't create dir %v: %w", d, err)
+		}
+	}
+	inboxPath := path.Join(baseDir, "inbox.md")
+	f, err := os.OpenFile(inboxPath,
+		os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return fmt.Errorf("can't create %v: %w", inboxPath, err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("can't close %v: %w", inboxPath, err)
+	}
+	return nil
+}
+
 // tasksCmd represents the tasks command
 var tasksCmd = &cobra.Command{
 	Use:   "tasks",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("tasks called")
+	Short: "Display all open tasks sorted by priority",
+	Long: ``,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := initBaseDir(); err != nil {
+			return err
+		}
+		fmt.Println(baseDir)
+		return nil
 	},
 }
 
